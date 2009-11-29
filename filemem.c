56867629751a07f9abefec318e5455434ad0f26b
brunof <bipus24hs@hotmail.com>
2009-11-29 20:32:32 -0300

    Basic format function added...

--- filemem.c
+++ filemem.c
@@ -1,41 +1,39 @@
 #include "filemem.h"
+#include "sysdef.h"
+#include <24256.C>
 
-//[ size | serial_number | block_size | free_block_start | index_block_start | data_block_start | system_id ]
-short format_memory(char mem_size, int32 ser_num, int16 blck_size, char* sys_id){
-
+//[ size | serial_number | block_size | free_block_start | free_block_end | index_block_start | index_block_end | data_block_start | data_block_end | system_id ]
+//short eepromfs_format(char mem_size, int32 ser_num, int16 blck_size, char* sys_id){
+short eepromfs_format(char * sys_id){
    int16 i;
-   //write memory size...
-   write_ext_eeprom(MEMORY_SIZE_START,mem_size);
-   
-   //write serial number...
-   write_ext_eeprom(SERIAL_NUMBER_START,make8(ser_num,0));
-   write_ext_eeprom(SERIAL_NUMBER_START+1,make8(ser_num,1));
-   write_ext_eeprom(SERIAL_NUMBER_START+2,make8(ser_num,2));
-   write_ext_eeprom(SERIAL_NUMBER_START+3,make8(ser_num,3));
-  
-   //write block size start
-   write_ext_eeprom(BLOCK_SIZE_START,make8(BLOCK_SIZE,0));
-   write_ext_eeprom(BLOCK_SIZE_START+1,make8(BLOCK_SIZE,1));
-   
-   //write free block start
-   write_ext_eeprom(FREE_BLOCK_START,make8(FREE_BLOCK,0));
-   write_ext_eeprom(FREE_BLOCK_START+1,make8(FREE_BLOCK,1));
-   
-   //write index block start
-   write_ext_eeprom(INDEX_BLOCK_START,make8(INDEX_BLOCK,0));
-   write_ext_eeprom(INDEX_BLOCK_START+1,make8(INDEX_BLOCK,1));
-    
-   //write data block start
-   write_ext_eeprom(DATA_BLOCK_START,make8(DATA_BLOCK,0));
-   write_ext_eeprom(DATA_BLOCK_START+1,make8(DATA_BLOCK,1));       
-    
-   //write system ID
-   i=SYSTEM_ID_START-1;
-   sys_id--;
-   do{
-      sys_id++;
-      i++;
-      write_ext_eeprom(i,*sys_id); 
-   }while(*sys_id!='\0');    
 
-};
+   init_ext_eeprom();
+
+   //write memory struct values...
+   for(i=0;i<sysDefaultValues_len;i++)
+   {
+      write_ext_eeprom(i,sysDefaultValues[i]);
+      delay_ms(5);
+   }
+
+   //now, format blocks:
+   
+   //free block FORMAT:
+   for(i=FREE_BLOCK_START;i<=FREE_BLOCK_END;i++)
+   {
+      write_ext_eeprom(i,0x00);
+      delay_ms(5);
+   }
+   
+   //index block FORMAT:
+   for(i=INDEX_BLOCK_START;i<=INDEX_BLOCK_END;i++)
+   {
+      write_ext_eeprom(i,0x00);
+      delay_ms(5);
+   }
+   
+   //data block shouldnt need to be formated
+
+   //success!
+   return TRUE;
+}
--- filemem.h
+++ filemem.h
@@ -1,10 +1,11 @@
 #ifndef  filemem_h
 #define  filemem_h
 
-//[ size | serial_number | block_size | free_block_start | index_block_start | data_block_start | system_id ]
+#define EEPROM_SDA  PIN_C4
+#define EEPROM_SCL  PIN_C3
 
-short format_memory(char mem_size, int32 ser_num, int16 blck_size, char* sys_id);
-
-
+//[ size | serial_number | block_size | free_block_start | free_block_end | index_block_start | index_block_end | data_block_start | data_block_end | system_id ]
+//short eepromfs_format(char mem_size, int32 ser_num, int16 blck_size, char* sys_id);
+short eepromfs_format(char * sys_id);
 
 #endif
